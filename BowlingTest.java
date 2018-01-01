1897a2288db58100df96c61d073ee8973d19b8f9
Tarhi Mohamed Redouane <red.skull913@outlook.com>
2018-01-01 23:05:11 +0100

    adding the strike algorithm + refactoring

--- BowlingTest.java
+++ BowlingTest.java
@@ -13,23 +13,23 @@ public class BowlingTest {
         game = new Game();
     }
 
-    private void rollMany(int repitition, int pins) {
-        for (int i = 0; i < repitition; i++) {
+    private void rollMany(int repetition, int pins) {
+        for (int i = 0; i < repetition; i++) {
             game.roll(pins);
         }
     }
 
     @Test
-    public void gutterGame()throws Exception{
+    public void gutterGame() throws Exception {
 
         rollMany(20, 0);
-        Assert.assertEquals(0,game.score());
+        Assert.assertEquals(0, game.score());
     }
 
     @Test
     public void allOnes() throws Exception {
         rollMany(20, 1);
-        Assert.assertEquals(20,game.score());
+        Assert.assertEquals(20, game.score());
     }
 
 
@@ -38,7 +38,22 @@ public class BowlingTest {
         game.roll(5);
         game.roll(5); //spare
         game.roll(3);
-        rollMany(17,0);
-        Assert.assertEquals(16,game.score());
+        rollMany(17, 0);
+        Assert.assertEquals(16, game.score());
+    }
+
+    @Test
+    public void oneStrike() throws Exception {
+        game.roll(10);//strike
+        game.roll(3);
+        game.roll(4);
+        rollMany(16,0);
+        Assert.assertEquals(24,game.score());
+    }
+
+    @Test
+    public void allStrikes() throws Exception {
+rollMany(12,10);
+Assert.assertEquals(300,game.score());
     }
 }
--- Game.java
+++ Game.java
@@ -1,22 +1,68 @@
 package BowlingGame;
 
 public class Game {
-    private int rolls[] = new  int[21];
-    private int currentRoll =0;
+    private int rolls[] = new int[21];
+    private int currentRoll = 0;
+    private int firstInFrame = 0;
+    private int score = 0;
+
     public void roll(int pins) {
-        rolls[currentRoll++]=pins;
+        rolls[currentRoll++] = pins;
     }
 
     public int score() {
-        int score = 0;
-        int i = 0;
-        for (int frame = 0 ; frame <10;frame++) {
-            if(rolls[i] + rolls[i+1]==10)//spare
-                score+=rolls[i+2];
-            score+= rolls[i]+rolls[i+1];
-            i+=2;
+        for (int frame = 0; frame < 10; frame++) {
+            if (isSpare(firstInFrame))
+                calculateScoreForSpare();
+            else if (isStrike(firstInFrame))
+                calculateScoreForStrike();
+            else
+                calculateScoreForFrame();
         }
-        return  score;
+        return score;
+    }
+
+    private void calculateScoreForFrame() {
+        score += getScoreForCurrentFrame(firstInFrame);
+        nextFrame();
+    }
+
+    private void calculateScoreForStrike() {
+        score += 10 + nextTwoBallsInFrame(firstInFrame);
+        nextFrameInCaseOfStrike();
+    }
+
+    private void calculateScoreForSpare() {
+        score += 10 + nextBallInFrame(firstInFrame);
+        nextFrame();
+    }
+
+    private void nextFrameInCaseOfStrike() {
+        firstInFrame++;
+    }
+
+    private void nextFrame() {
+        firstInFrame += 2;
+    }
+
+    private int getScoreForCurrentFrame(int firstInFrame) {
+        return rolls[firstInFrame] + rolls[firstInFrame + 1];
+    }
+
+    private int nextBallInFrame(int firstInFrame) {
+        return rolls[firstInFrame + 2];
+    }
+
+    private int nextTwoBallsInFrame(int firstInFrame) {
+        return rolls[firstInFrame + 1] + rolls[firstInFrame + 2];
+    }
+
+    private boolean isStrike(int i) {
+        return rolls[i] == 10;
+    }
+
+    private boolean isSpare(int i) {
+        return rolls[i] + rolls[i + 1] == 10;
     }
 
 }
